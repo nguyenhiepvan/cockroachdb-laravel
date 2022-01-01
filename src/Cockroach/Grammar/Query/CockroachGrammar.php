nguyenhiepvan/cockroachdb-laravel

commit 5048ff816e796e081848b1668833e05a581c6b71

Remove schema/table name from the update columns key
Using the schema name for a column generates an error within Cockroach:

SQLSTATE[0A000]: Feature not supported: 7 ERROR:  at or near \"table\": syntax error: unimplemented
HINT:  You have attempted to use a feature that is not yet implemented. See: https://go.crdb.dev/issue-v/27792/v21.1

The update column name is automatically generated by eloquent for columns such as timestamps as '"schema"."table"."updated_at", which is invalid for Postgres and Cockroach.

Similar to issues seen in https://github.com/laravel/framework/issues/13707

src/Cockroach/Grammar/Query/CockroachGrammar.php
@@ -131,7 +131,7 @@ class CockroachGrammar extends Grammar
         // columns and convert it to a parameter value. Then we will concatenate a
         // list of the columns that can be added into this update query clauses.
         return collect($values)->map(function ($value, $key) {
-            return $this->wrap($key).' = '.$this->parameter($value);
+            return $this->wrap(end(explode('.', $key))).' = '.$this->parameter($value);
         })->implode(', ');
     }
 

commit 16a0e063c37018124061adaf2a5a8166fff3c44e

end() needs to be passed by reference - fixes issue introduced by 5048ff8

src/Cockroach/Grammar/Query/CockroachGrammar.php
@@ -131,7 +131,8 @@ class CockroachGrammar extends Grammar
         // columns and convert it to a parameter value. Then we will concatenate a
         // list of the columns that can be added into this update query clauses.
         return collect($values)->map(function ($value, $key) {
-            return $this->wrap(end(explode('.', $key))).' = '.$this->parameter($value);
+            $column = explode('.', $key);
+            return $this->wrap(end($column)).' = '.$this->parameter($value);
         })->implode(', ');
     }
 